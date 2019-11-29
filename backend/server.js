7d81068ae219a2d840bf6f1a9626bfc8241d3563
Stephanie Viveros <sviveros05@gmail.com>
2019-11-29 17:31:21 -0600

    Configuered Server to work with dotenv file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
-// require('dotenv').config
+require('dotenv').config()
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const methodOverride = require('method-override');
-const PORT = 5000
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const methodOverride = require('method-override')
+const PORT = process.env.PORT;
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 
-const app = express()
+app.use('/', (req, res) => {
+	res.send('Hello World')
+})
 
 app.listen(PORT, () => {
 	console.log(`SERVER IS RUNNING ON PORT: ${PORT}`);
