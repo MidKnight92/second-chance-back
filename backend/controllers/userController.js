3973d46e3b5ac06092d345fb1b6fda8e61d44156
Stephanie Viveros <sviveros05@gmail.com>
2019-11-29 18:44:20 -0600

    Pseudocoded User routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,4 +4,25 @@ const User = require('../models/user.js')
 const Dog = require('../models/dog.js')
 const bcrypt = require('bcryptjs')
 
+// User Registration -
+// GET /users/register
+// POST /users/register
+
+// User Login
+// GET /users/login
+// POST /users/login
+
+// User Edit - require Auth This route will allow them to edit their personal information
+// GET /users/:id/edit
+// PUT /users/:id/
+
+// User Logout
+// GET /users/logout
+
+// User Delete - require Auth
+// DELETE /users/:id
+
+// Users Show Page: This route shows users dog matches 
+//GET /users/:id
+
 module.exports = router
